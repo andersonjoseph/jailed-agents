318258cd57528e82015e07f2abfe96a99e6248f0
anderson <andersonjoseph@mailfence.com>
2025-12-27 19:32:28 -0400

    add opencode

--- flake.nix
+++ flake.nix
@@ -15,42 +15,61 @@
       config.allowUnfree = true;
     };
     jail = jail-nix.lib.init pkgs;
-    crush-pkg = llm-agents.packages.${system}.crush;
 
-    makeJailedCrush = { extraPkgs ? [] }: jail "jailed-crush" crush-pkg (with jail.combinators; [
+    crush-pkg = llm-agents.packages.${system}.crush;
+    opencode-pkg = llm-agents.packages.${system}.opencode;
+
+    commonPkgs = with pkgs; [
+      bashInteractive
+      curl
+      wget
+      jq
+      git
+      which
+      ripgrep
+      gnugrep
+      gawkInteractive
+      ps
+      findutils
+      gzip
+      unzip
+      gnutar
+      diffutils
+    ];
+
+    commonJailOptions = with jail.combinators; [
 	network
 	time-zone
 	no-new-session
-
 	mount-cwd
+    ];
 
+    makeJailedCrush = { extraPkgs ? [] }: jail "jailed-crush" crush-pkg (with jail.combinators; (
+	commonJailOptions ++ [
 	(readwrite (noescape "~/.config/crush"))
 	(readwrite (noescape "~/.local/share/crush"))
 
-	(add-pkg-deps [
-	  pkgs.bashInteractive
-	  pkgs.curl
-	  pkgs.wget
-	  pkgs.jq
-	  pkgs.git
-	  pkgs.which
-	  pkgs.ripgrep
-	  pkgs.gnugrep
-	  pkgs.gawkInteractive
-	  pkgs.ps
-	  pkgs.findutils
-	  pkgs.gzip
-	  pkgs.unzip
-	  pkgs.gnutar
-	  pkgs.diffutils
-	])
+	(add-pkg-deps commonPkgs)
 
 	(add-pkg-deps extraPkgs)
-    ]);
+    ]));
+
+    makeJailedOpencode = { extraPkgs ? [] }: jail "jailed-opencode" opencode-pkg (with jail.combinators; (
+	commonJailOptions ++ [
+	(readwrite (noescape "~/.config/opencode"))
+	(readwrite (noescape "~/.local/share/opencode"))
+	(readwrite (noescape "~/.local/state/opencode"))
+
+	(add-pkg-deps commonPkgs)
+
+	(add-pkg-deps extraPkgs)
+    ]));
+
   in 
   {
     lib = {
       inherit makeJailedCrush;
+      inherit makeJailedOpencode;
     };
 
     devShells.default = pkgs.mkShell {
@@ -58,6 +77,7 @@
 	pkgs.nixd
 
 	(makeJailedCrush {})
+	(makeJailedOpencode {})
       ];
     };
   });
